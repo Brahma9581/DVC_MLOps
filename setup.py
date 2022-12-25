fe043f16d5cc05b54c8e1736534a07bdbee041d2
Brahmanayudu <brahma.danna@gmail.com>
2022-12-25 19:39:43 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Brahma9581",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Brahma9581/DVC_MLOps",
+    author_email="brahma.danna@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
